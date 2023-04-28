c2ef3dbe8e5733feecb58bf5278cc90df7818771
kirtfieldk <kirtfieldk@mymail.vcu.edu>
2023-04-28 08:28:35 -0400

    Initial Commit

---
+++ Astella/AstellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstellaApp.swift
+//  Astella
+//
+//  Created by Keith Kirtfield on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AstellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
